36c30a07881a25f8531f50482b532c2a2694225f
Florian Becker <becker_florian@online.de>
2021-11-06 16:33:14 +0100

    created the hobbies.routes, some preparation for other routes

---
+++ routes/hobbies.routes.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const Hobby = require("../models/Hobby.model");
+
+// create the main hobbies route (list)
+
+router.get("/", (req, res, next) => {
+//   Hobby.find({}, { hobbyImage: 1, name: 1, typeOfActivity: 1 })
+  
+Hobby.find()
+    .then((data) => res.json(data))
+    .catch((err) => next(err));
+});
+
+// create the add hobbies route
+
+router.post("/create", (req, res, next) => {
+  const { name, typeOfActivity, description, hobbyImage, placeOfActivity } = req.body;
+  // post_id -> not needed here?
+  // console.log(req.body);
+  console.log("CREATE HOBBIES")
+  Hobby.create({
+    name,
+    typeOfActivity,
+    description,
+    hobbyImage,
+    placeOfActivity,
+    // post_id, -> not needed here?
+  })
+    .then((data) => res.json(data))
+    .catch((err) => next(err));
+});
+
+// create the detailed hobbies route
+
+router.get("/:id", (req, res, next) => {
+    Hobby.findById(req.params.id)
+    .then((data) => res.json(data))
+    .catch((err) => next(err));
+})
+
+// create the edit hobbies route
+// router.patch because patch will only update the specific/chosen hobby _> /:id 
+// (!) setting the change to "true" to confirm the done changes
+
+router.patch("/:id", (req, res, next) => {
+    const { name, typeOfActivity, description, hobbyImage, placeOfActivity } = req.body;
+    Hobby.findByIdAndUpdate(
+        req.params.id,
+        { name, typeOfActivity, description, hobbyImage, placeOfActivity },
+        { new: true }
+    )
+    .then((data) => res.json(data))
+    .catch((err) => next(err));
+})
+
+// create the delete hobbies route
+// router.delete
+// after deleting the hobby => redirect to hobby list ("/") ?
+
+router.delete("/:id", (req, res, next) => {
+    Hobby.findByIdAndDelete(req.params.id)
+    .then((data) => res.json(data._id))
+    .catch((err) => next(err));
+})
+
+module.exports = router;
