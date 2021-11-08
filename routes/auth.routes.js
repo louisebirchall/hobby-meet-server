14dee82f619249672dc18fae7d2282cb922e9b50
Florian Becker <becker_florian@online.de>
2021-11-08 20:13:14 +0100

    continued with auth route (profile/s)

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,14 +8,21 @@ router.post("/signup", (req, res, next) => {
 
   // verify sent info
   if (!username || !email || !password) {
-    return res.status(400).json({ errorMessage: "Hey! You need to enter your username, email and password for sign in!" });
+    return res
+      .status(400)
+      .json({
+        errorMessage:
+          "Hey! You need to enter your username, email and password for sign in!",
+      });
   }
 
   // email BE validation
-  const mailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const mailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (!mailRegex.test(email)) {
     return res.json({
-      errorMessage: "Wooho! that seems to be an incorrect type of email. Please, write a correct one!",
+      errorMessage:
+        "Wooho! that seems to be an incorrect type of email. Please, write a correct one!",
     });
   }
 
@@ -23,36 +30,37 @@ router.post("/signup", (req, res, next) => {
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
   if (!passwordRegex.test(password)) {
     return res.json({
-      errorMessage: "Oooo your password needs to be, at least, 8 characters long, uppercase and lowercase, if you want to get in!",
+      errorMessage:
+        "Oooo your password needs to be, at least, 8 characters long, uppercase and lowercase, if you want to get in!",
     });
   }
 
   // check if username is taken
-  User.findOne({ username: username })
-    .then((founduser) => {
-      if (founduser) {
-        return res.json({
-          errorMessage: "Hi fellow! This username has already been taken. We are sure you'll find another one that fits you!",
-        });
-      }
+  User.findOne({ username: username }).then((founduser) => {
+    if (founduser) {
+      return res.json({
+        errorMessage:
+          "Hi fellow! This username has already been taken. We are sure you'll find another one that fits you!",
+      });
+    }
     // encrypt password
     const saltRounds = 10;
     return bcrypt
       .genSalt(saltRounds)
       .then((salt) => bcrypt.hash(password, salt))
       .then((hashedPassword) => {
-          // create user
-          return User.create({ username, password: hashedPassword });
-        })
+        // create user
+        return User.create({ username, password: hashedPassword });
+      })
       .then((user) => {
-          req.session.user = user;
-          res.status(201).json(user);
-        })
+        req.session.user = user;
+        res.status(201).json(user);
+      })
       .catch((error) => {
-          return res.json({
-            errorMessage: `Problem creating user, ${error.message}`,
-          });
-        })
+        return res.json({
+          errorMessage: `Problem creating user, ${error.message}`,
+        });
+      });
   });
 });
 
@@ -62,7 +70,9 @@ router.post("/login", (req, res, next) => {
 
   if (!username || !password) {
     return res.status(400).json({
-      errorMessage: `Maybe did you forget to fill something? ${username ? 'password' : password ? 'username and password' : 'username'}`,
+      errorMessage: `Maybe did you forget to fill something? ${
+        username ? "password" : password ? "username and password" : "username"
+      }`,
     });
   }
 
@@ -70,7 +80,8 @@ router.post("/login", (req, res, next) => {
     .then((user) => {
       if (!user) {
         return res.json({
-          errorMessage: "Do you have an account yet? Make sure you write you username correctly :-D!",
+          errorMessage:
+            "Do you have an account yet? Make sure you write you username correctly :-D!",
         });
       }
       bcrypt.compare(password, user.password).then((isSamePassword) => {
@@ -83,7 +94,9 @@ router.post("/login", (req, res, next) => {
         return res.json(user);
       });
     })
-    .catch((err) => {next(err)});
+    .catch((err) => {
+      next(err);
+    });
 });
 
 // logout
@@ -94,7 +107,9 @@ router.post("/logout", (req, res, next) => {
         .status(500)
         .json({ errorMessage: `Problem with logout ${error.message}` });
     }
-    res.json({ successMessage: "We'll miss you! We hope you'll come back soon!" });
+    res.json({
+      successMessage: "We'll miss you! We hope you'll come back soon!",
+    });
   });
 });
 
@@ -103,7 +118,67 @@ router.get("/loggedin", (req, res, next) => {
   if (req.session.user) {
     res.json({ user: req.session.user });
   }
-  res.status(403).json({ errorMessage: "There was a problem with authentication. Please, try again!" });
+  res
+    .status(403)
+    .json({
+      errorMessage:
+        "There was a problem with authentication. Please, try again!",
+    });
+});
+
+//profiles (list to search)
+router.get("/profiles", (req, res, next) => {
+  User.find()
+    .then((data) => res.json(data))
+    .catch((err) => next(err));
+});
+
+//detailed profile (=> private)
+router.get("/profile/:id", (req, res, next) => {
+  const logged_id = req.session.user._id;
+  const isLoggedUser = user_id === logged_id;
+  User.findById(req.params.id)
+    .then((data) => res.json(data, isLoggedUser))
+    .catch((err) => next(err));
+});
+
+//edit profile
+router.patch("/profile/:id", (req, res, next) => {
+  const {
+    username,
+    email,
+    fullName,
+    profileImage,
+    sex,
+    age,
+    hobbies,
+    typeOfUser,
+    isAdmin,
+  } = req.body;
+  User.findByIdAndUpdate(
+    req.params.id,
+    {
+    username,
+    email,
+    fullName,
+    profileImage,
+    sex,
+    age,
+    hobbies,
+    typeOfUser,
+    isAdmin,
+    },
+    { new: true }
+  )
+    .then((data) => res.json(data))
+    .catch((err) => next(err));
+});
+
+//delete profile
+router.delete("/profile/:id", (req, res, next) => {
+  User.findByIdAndDelete(req.params.id)
+    .then((data) => res.json(data._id))
+    .catch((err) => next(err));
 });
 
 module.exports = router;
