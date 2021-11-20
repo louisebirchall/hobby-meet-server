82c072739cf0790d6e6c40776df6f8a98f95c647
calonsogarcia <carolina.alon.garcia@gmail.com>
2021-11-20 16:29:20 +0100

    loggedin things

--- middlewares/authoritation.js
+++ middlewares/authoritation.js
@@ -4,7 +4,7 @@ module.exports = {
         if (req.session.user) {
           next()
         } else {
-          res.status(403).json({message: "You don't have and user, so you can't loggin"})
+          res.status(403).json({message: "You don't have and user, so you can't do this"})
         }
       },
 
