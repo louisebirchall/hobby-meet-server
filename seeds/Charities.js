ddbcc8623513fe5012413f7d947bd8886449c074
louise <louise.birchall@gmx.com>
2021-11-27 11:53:57 +0100

    seeds

--- seeds/Charities.js
+++ seeds/Charities.js
@@ -3,55 +3,34 @@ const Charity = require("../models/Charity.model");
 
 require("../db");
 
-const events = [
+const charities = [
   {
     image:
-      "https://media.istockphoto.com/photos/sewing-class-picture-id518831826?k=20&m=518831826&s=612x612&w=0&h=rYaSLvWwTh55-oEDIJZtH-c7pUciduWkF1qkSGhgY9c=",
-    name: "Adopta",
-    description:
-      "For beginners to discuss any questions they may have and more experienced sewers to offer tips and advice",
-    date: "07/12/2021",
-    attendees: "4",
-    attendees_max: "12",
-    attendees_min: "2",
-    pricePolicy: "Free",
-    location: "Zoom, link to follow",
+      "https://scontent.fvlc6-2.fna.fbcdn.net/v/t1.6435-9/120088167_126103599233246_283390310082932307_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=lWvskISA5ygAX-pw3FL&_nc_ht=scontent.fvlc6-2.fna&oh=33676cbd1957ec7be44a4857adfe63b2&oe=61C71779",
+    name: "Adopta Gats Valencia",
+    description: "Cats, everywhere, and they all need homes",
   },
   {
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/%E3%83%9C%E3%83%AB%E3%83%80%E3%83%AA%E3%83%B3%E3%82%B0_%287955003588%29.jpg/220px-%E3%83%9C%E3%83%AB%E3%83%80%E3%83%AA%E3%83%B3%E3%82%B0_%287955003588%29.jpg",
-    title: "Bouldering Thursdays",
-    description:
-      "Come along and give it a try, or compete with others. All covid protocols are being followed",
-    date: "07/12/2021",
-    attendees: "6",
-    attendees_max: "20",
-    attendees_min: "2",
-    pricePolicy: "Free",
-    location: "The Bunker",
+      "https://d1kvlp4er3agpe.cloudfront.net/resources/images/groups/0/3/9/9/0/720_nfymsrdiei.jpg",
+    title: "Dog Rescue Madrid",
+    description: "Helping dogs in need in the Madrid area",
   },
   {
     image:
-      "https://picturecorrect-wpengine.netdna-ssl.com/wp-content/uploads/2018/12/perspective-image.jpg",
-    title: "Perspective Workshop",
-    description:
-      "Come and learn about perspective, how to frame photos and get the best images",
-    date: "07/12/2021",
-    attendees: "7",
-    attendees_max: "10",
-    attendees_min: "2",
-    pricePolicy: "Free",
-    location: "Zoom, link to follow",
+      "https://daniel-barenboim-stiftung.org/cdn/Media/Media//file-daniel-barenboim---musikalische-schule---credit--peter-adamik-ot5rbqwn43.jpg",
+    title: "Daniel Barenboim Stiftung",
+    description: "Helping children through music",
   },
 ];
 
-Event.create(events)
-  .then((createdEvents) => {
-    console.log(`Created ${createdEvents.length} events`);
+Charity.create(charities)
+  .then((createdCharities) => {
+    console.log(`Created ${createdCharities.length} charities`);
 
     // once they're created, close the connection
     mongoose.connection.close();
   })
   .catch((error) =>
-    console.log("An error occurred on the event creation", error)
+    console.log("An error occurred on the charity creation", error)
   );
